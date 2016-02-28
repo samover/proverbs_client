caa0e1d570817879eb562e235d9c7ff10fdbd019
samover <samuel.overloop@gmail.com>
2016-02-28 20:31:14 +0100

    simplfies design

--- public/js/app.js
+++ public/js/app.js
@@ -1,7 +1,8 @@
 $(document).ready(function() {
   var QUOTE_API = 'http://localhost:8080/api/'
   var isLoggedIn = true;
-  ran_col();
+  getQuote();
+
   if(isLoggedIn) { 
     $('#navigation').load('views/navigation/login.html');
   } else {
@@ -12,20 +13,12 @@ $(document).ready(function() {
     e.stopPropagation();
   });
 
-  $('#new_quote').on('click', function() {
-    ran_col();
-    $.getJSON(QUOTE_API + 'quote', function(data) {
-      console.log(data);
-      $('blockquote p').html(data.text);
-      $('p.quote-author').html(data.authors[0]);
+  $('.new_quote').on('click', getQuote);
+
+  function getQuote() {
+    $.getJSON(QUOTE_API + 'proverb', function(data) {
+      var proverb = data.proverb.replace(/\n/, '<br>');
+      $('blockquote p').html(proverb);
     });
-  });
-
-  function ran_col() { 
-    var color = '#'; 
-    var letters = ['000000','FF0000','00FF00','0000FF','FFFF00','00FFFF','FF00FF','C0C0C0'];
-    color += letters[Math.floor(Math.random() * letters.length)];
-    $('body').css('background-color', color);
   }
-
 });
