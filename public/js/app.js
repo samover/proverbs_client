2276154458b41616983c77e72ff5cfbbd47e8dfe
samover <samuel.overloop@gmail.com>
2016-02-29 13:44:04 +0100

    add twitter button

--- public/js/app.js
+++ public/js/app.js
@@ -1,12 +1,20 @@
 $(document).ready(function() {
+  var proverb;
   var QUOTE_API = 'https://proverbs-api.herokuapp.com/api/'
   getQuote();
+
+  $('#tweet').on('click', function() {
+    var params = 'text=' + proverb
+                + '&hashtags=wisdom'
+                + '&url=http://goo.gl/s7mHTR';
+    window.open('https://twitter.com/intent/tweet?' + params, null, "height=200,width=400,status=yes,toolbar=no,menubar=no,location=no");
+  });
 
   $('.new_quote').on('click', getQuote);
 
   function getQuote() {
     $.getJSON(QUOTE_API + 'proverb', function(data) {
-      var proverb = data.proverb.replace(/\n/, '<br>');
+      proverb = data.proverb.replace(/\n/, '<br>');
       $('blockquote p').html(proverb);
     });
   }
